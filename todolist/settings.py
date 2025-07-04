d7585ef4d8b798944c66305cb3f1aef0251687cb
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-07-04 10:45:59 +0300

    Solution

--- todolist/settings.py
+++ todolist/settings.py
@@ -62,12 +62,15 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': '1234',
-        'HOST': '172.17.0.2',  # You can use a different host if your MySQL server is on a remote machine.
-        'PORT': '',  # Leave this empty to use the default MySQL port (3306).
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'todolist'),
+        'USER': os.getenv('DB_USER', 'todouser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'todopass'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
